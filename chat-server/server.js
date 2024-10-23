0441f488e85d1fd3110c64029aa6dfb0420b11a7
AdityaKumar2408 <adityachouhan7705@gmail.com>
2024-10-23 18:35:00 +0530

    update changes

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,3 +1,47 @@
+// require('dotenv').config(); 
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(cors());
+
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "https://bespoke-bubblegum-37956b.netlify.app", // Netlify URL
+//     methods: ["GET", "POST"]
+//   }
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Server is running perfectly!');
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('New user connected');
+
+//   socket.on('joinGroup', ({ name, groupId }) => {
+//     socket.join(groupId);
+//     io.to(groupId).emit('message', `${name} has joined the group`);
+//   });
+
+//   socket.on('sendMessage', ({ name, message, groupId }) => {
+//     io.to(groupId).emit('message', `${name}: ${message}`);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected');
+//   });
+// });
+// const PORT = process.env.PORT || 5000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
 require('dotenv').config(); 
 const express = require('express');
 const http = require('http');
@@ -10,8 +54,11 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://bespoke-bubblegum-37956b.netlify.app", // Netlify URL
-    methods: ["GET", "POST"]
+    // origin: ["https://bespoke-bubblegum-37956b.netlify.app", "http://localhost:3001"], // Netlify URL
+    // origin: ["http://localhost:3001","http://localhost:3000","https://capable-syrniki-760b81.netlify.app/"], // Netlify URL
+    origin: "http://localhost:3001", // Netlify URL
+    methods: ["GET", "POST"],
+    credentials:true
   }
 });
 
@@ -28,7 +75,8 @@ io.on('connection', (socket) => {
   });
 
   socket.on('sendMessage', ({ name, message, groupId }) => {
-    io.to(groupId).emit('message', `${name}: ${message}`);
+    // io.to(groupId).emit('message', `${name}: ${message}`);
+    io.to(groupId).emit('message', { name, message, from: socket.id });
   });
 
   socket.on('disconnect', () => {
